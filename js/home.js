b07b7442286c2a1d66d046bca1b5843db3239c50
Roxana Cristurean <roxana_cristurean@yahoo.com>
2020-01-09 19:44:28 +0200

    Home customization

--- js/home.js
+++ js/home.js
@@ -1,44 +1,26 @@
 $(document).ready(function() {
     let playing = false;
     $('#s').click(function() {
-        let intro = new Audio("sound/intro.mp3");
-        intro.addEventListener("ended", function(){
-            playing=false;
-            window.open("letter_s.html");
-            // window.location.href = "letter_s.html";
-
-        });
-        if (!playing) {
-            playing = true;
-            intro.play();
-        }
+            window.location.href = "sentences.html";
     });
 
     $('#digits').click(function() {
         let intro = new Audio("sound/intro_cifre.m4a");
         intro.addEventListener("ended", function(){
             playing=false;
-            window.open("learn_digits.html");
-            // window.location.href = "learn_digits.html";
+            window.location.href = "learn_digits.html";
         });
         if (!playing) {
             playing = true;
             intro.play();
         }
     });
-    $('#ghiozdanInitial').click(function() {
-        window.open("invatareRechizite.html");
-        // window.location.href = "invatareRechizite.html";
+    $('#backPack').click(function() {
+        window.location.href = "invatareRechizite.html";
     });
 
-    $('#syllable-dot').click(function() {
-        window.open("syllables.html");
-        // window.location.href = "syllables.html";
-    });
-    
-    $('#sentences').click(function() {
-        window.open("sentences.html");
-        // window.location.href = "sentences.html";
+    $('#story').click(function() {
+        window.location.href = "story.html";
     });
 
 });
