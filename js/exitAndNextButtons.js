b851d26ded0ad86676556b78ebfdb5e820685431
Roxana Cristurean <roxana_cristurean@yahoo.com>
2020-01-09 19:04:02 +0200

    Next and exit buttons added

---
+++ js/exitAndNextButtons.js
@@ -0,0 +1,13 @@
+
+function home(){
+    location.href='home.html';
+}
+
+function goToSyllables(){
+    location.href="syllables.html";
+}
+
+function goToSounds(){
+    location.href="letter_s.html";
+}
+
